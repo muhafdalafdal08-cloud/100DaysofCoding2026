b035de71fcfffab4f8b410af8905cd54e418b185
dall368 <muhafdalafdal08@gmail.com>
2026-09-03 01:15:29 +0800

    Update Day02.java

--- Day02.java
+++ Day02.java
@@ -3,23 +3,23 @@ public class Biodata {
 
         // Menggunakan println()
         System.out.println("=== BIODATA DIRI ===");
-        System.out.println("Nama       : muh afdhal asysyifa");
+        System.out.println("Nama       : Muh afdhal asysyifa");
         System.out.println("Prodi      : Informatika");
 
         // Menggunakan print()
         System.out.print("Asal Kota  : ");
-        System.out.print("Majene");
-        System.out.print("prodi");
+        System.out.print("kabupaten pasangkayu");
+        System.out.print("\n");
 
-        // Menggunakan string()
-        String nama = "muh afdhal asysyifa";
+        // Menggunakan printf()
+        String nama = "Muh afdhal asysyifa";
         String prodi = "Informatika";
         String kota = "kabupaten pasangkayu";
         int umur = 18;
 
-        System.out.printf("Nama       : muh afdhal asysyifa", nama);
-        System.out.printf("Prodi      : informatika", prodi);
-        System.out.printf("Asal Kota  : kabupaten pasangkayu", kota);
-        System.out.printf("Umur       : 18", umur);
+        System.out.printf("Nama       : %s%n", nama);
+        System.out.printf("Prodi      : %s%n", prodi);
+        System.out.printf("Asal Kota  : %s%n", kota);
+        System.out.printf("Umur       : %d tahun%n", umur);
     }
 }
