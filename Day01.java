0d3d8419c10df2a45e9c068bd4fce2f222fece46
dall368 <muhafdalafdal08@gmail.com>
2026-09-02 20:16:33 +0800

    Update and rename Days01ofcoding2026 to Day01.java

---
+++ Day01.java
@@ -0,0 +1,19 @@
+/**
+ * Contoh penggunaan komentar dalam Java.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Menampilkan nama pengguna
+        String nama = "afdal";
+
+        /*
+         * Menampilkan pesan ke layar.
+         * Program akan mengambil nilai
+         * dari variabel nama.
+         */
+        System.out.println("Halo, " + nama);
+
+    }
+}
