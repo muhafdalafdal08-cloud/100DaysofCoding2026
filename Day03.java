eb6a751dac3b83e00dc57608d8b0a5d16a500a3e
dall368 <muhafdalafdal08@gmail.com>
2026-09-04 00:14:00 +0800

    Day03.java

---
+++ Day03.java
@@ -0,0 +1,19 @@
+public class StrukBelanja {
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println("\tSTRUK BELANJA");
+        System.out.println("================================");
+
+        System.out.println("Nama\t\tHarga");
+        System.out.println("Buku\t\tRp10.000");
+        System.out.println("Pulpen\t\tRp5.000");
+        System.out.println("Pensil\t\tRp3.000");
+
+        System.out.println("--------------------------------");
+        System.out.println("Total\t\tRp18.000\n");
+
+        System.out.println("Terima kasih sudah berbelanja!");
+        System.out.println("================================");
+    }
+}
