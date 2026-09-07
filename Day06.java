c40c64e74f5a8d67c7242cffc60723e40d2b337c
dall368 <muhafdalafdal08@gmail.com>
2026-09-07 22:04:48 +0800

    Day06.java

---
+++ Day06.java
@@ -0,0 +1,13 @@
+public class day6 {
+
+    public static void main(String[] args) {
+        
+        boolean data = (7>3);
+        char jeniskelamin = ('L');
+
+        System.out.println("apakah 7 lebih besar dari pada 3 =\t" + data);
+        System.out.println("jenis kelamin\t\t\t =\t" + jeniskelamin);
+
+    }
+
+}
