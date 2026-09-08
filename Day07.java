8f9dfbfdf1cdacc181379a8a9e2579e8fadc8e1f
dall368 <muhafdalafdal08@gmail.com>
2026-09-08 19:07:56 +0800

    Day07.java

---
+++ Day07.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+
+    String nama = "afdal";
+    String prodi = "informatika";
+    String alamat = "pasangkayu";
+    String kelas = "B";
+
+    System.out.println("nama    = " + nama);
+    System.out.println("prodi   = " + prodi);
+    System.out.println("alamat  = " + alamat);
+    System.out.println("kelas   = " + kelas);
+
+  }
+}
