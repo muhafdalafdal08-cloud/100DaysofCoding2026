bf48d1a2945c3d906b496007210e0b5b1613cede
dall368 <muhafdalafdal08@gmail.com>
2026-09-12 20:05:25 +0800

    Day11.java

---
+++ Day11.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class day11 {
+
+    public static void main(String[] args) {
+        
+        Scanner rendy = new Scanner(System.in);
+
+        System.out.println("masukkan nama:  ");
+        String nama = afdal.nextLine();
+        
+        System.out.println("Nama saya =\t" + nama);
+
+        // pengenalan scanner
+        /* Scanner adalah class dalam java yang digunakan untuk menerima 
+        atau membaca input dari pengguna,
+        terutama input yang diberikan keybord */
+
+    }
+}
