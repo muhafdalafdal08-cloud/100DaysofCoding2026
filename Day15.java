f203b1d7d8b5a5764ff184e7d030a6f0d72665f1
dall368 <muhafdalafdal08@gmail.com>
2026-09-16 20:48:45 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,12 @@
+public class Aritmatika {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+
+        int perkalian = a * b;
+        int pembagian = a / b;
+
+        System.out.println("Perkalian = " + perkalian);
+        System.out.println("Pembagian = " + pembagian);
+    }
+}
