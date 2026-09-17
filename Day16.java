f008da4b5ba5a3cb26ba2a90e50f2711e8423883
dall368 <muhafdalafdal08@gmail.com>
2026-09-17 19:34:50 +0800

    Day16.java

--- Day16.java
+++ Day16.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Modulus {
     public static void main(String[] args) {
-        int a = 17;
-        int b = 5;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan angka pertama: ");
+        int a = input.nextInt();
+
+        System.out.print("Masukkan angka kedua: ");
+        int b = input.nextInt();
 
         int sisa = a % b;
 
         System.out.println("Sisa bagi = " + sisa);
+
+        input.close();
     }
 }
