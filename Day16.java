2271bcbf13c5a2d4a76cb26b69edbd33ef909c4e
dall368 <muhafdalafdal08@gmail.com>
2026-09-17 19:29:28 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,10 @@
+public class Modulus {
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 5;
+
+        int sisa = a % b;
+
+        System.out.println("Sisa bagi = " + sisa);
+    }
+}
