6396b52f70594f041f4bf90bb1eb6c2641c1a97e
dall368 <muhafdalafdal08@gmail.com>
2026-09-21 19:00:15 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,11 @@
+public class PrimitifKeString {
+    public static void main(String[] args) {
+
+        int angka = 100;
+
+        String teks = String.valueOf(angka);
+
+        System.out.println("Angka = " + angka);
+        System.out.println("String = " + teks);
+    }
+}
