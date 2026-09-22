32e9a2c7f66b4d02ce14fb4576725244bf1234a5
dall368 <muhafdalafdal08@gmail.com>
2026-09-22 21:50:41 +0800

    Day21.java

---
+++ Day21.java
@@ -0,0 +1,11 @@
+public class StringKePrimitif {
+    public static void main(String[] args) {
+
+        String angka = "25";
+
+        int hasil = Integer.parseInt(angka);
+
+        System.out.println("String = " + angka);
+        System.out.println("Int = " + hasil);
+    }
+}
